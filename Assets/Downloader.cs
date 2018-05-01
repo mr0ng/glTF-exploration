62713165d2f3b632d5dd8857ddaa9045f83f2c64
WilliamSokolErhard <william.j.sokol@gmail.com>
2018-04-30 18:46:51 -0700

    Fully functional

---
+++ Assets/Downloader.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityGLTF;
+
+public class Downloader : MonoBehaviour
+{
+
+    public string url = null;
+
+    [SerializeField]
+    private bool loadOnStart = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (loadOnStart)
+        {
+            StartCoroutine(Download(url));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Download(string url)
+    {
+        Uri uri = new Uri(url);
+        string path = Path.Combine(Application.streamingAssetsPath.Replace('\\', '/') + "/", Path.GetFileName(uri.AbsolutePath));
+
+        WWW www;
+
+        byte[] file = null;
+
+        www = new WWW(url);
+
+        yield return www;
+        if (www.error == null)
+        {
+            file = www.bytes;
+            www.Dispose();
+            File.WriteAllBytes(path, file);
+            yield return File.Exists(path);
+            print("downloaded already: " + path);
+        }
+        else
+        {
+            Debug.Log(www.error);
+        }
+
+        //path = Path.ChangeExtension(path, ".bin");
+        
+        //url = Path.ChangeExtension(url, ".bin");
+
+        //file = null;
+
+        //www = new WWW(url);
+
+        //yield return www;
+        //if (www.error == null)
+        //{
+        //    file = www.bytes;
+        //    www.Dispose();
+        //    File.WriteAllBytes(path, file);
+        //    yield return File.Exists(path);
+        //    print("downloaded already: " + path);
+        //}
+        //else
+        //{
+        //    Debug.Log(www.error);
+        //}
+
+        GetComponent<GLTFComponent>().GLTFUri = Path.GetFileName(uri.AbsolutePath);
+        print(GetComponent<GLTFComponent>().GLTFUri);
+        StartCoroutine(GetComponent<GLTFComponent>().Load());
+    }
+}
